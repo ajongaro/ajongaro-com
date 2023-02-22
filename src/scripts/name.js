ab5389b2c907f80d902c546a8ddf3d4176082780
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-02-21 22:01:46 -0700

    Abstract Name to name.js

---
+++ src/scripts/name.js
@@ -0,0 +1,8 @@
+		const title = document.getElementById("title");
+
+		title.onmouseover = function() {
+			title.innerHTML = "Hi, I'm Anthony James Ongaro!";
+		};
+		title.onmouseout = function() {
+			title.innerHTML = "Hi, I'm Anthony J. Ongaro!";
+		};
